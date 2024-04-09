da0ff5df3de875722c5824c480ef67dce62803cf
Martorias <38153913+Martorias@users.noreply.github.com>
2024-04-09 22:09:31 +0200

    Update orca_sc.py
    wip

--- orca_stealthchanger/orca_sc.py
+++ orca_stealthchanger/orca_sc.py
@@ -1,22 +1,39 @@
-import sys
 import re
+import sys
 
-z_offset_adjust = True
-led_effects = True
+# Settings:
+cooldown_rows = 10000       # How many rows between same toolhead being used to call for cooldown
+cooldown_temp = 150         # Temperature to cool down to
+preheat_rows = 2000         # How many rows before toolchange to start pre-heating
+led_effects = True          # Use led effects (currently for 3x RGB, dragonburner)
+z_offset_adjust = True      # If gcode should be z-adjusted, check the end to change templates
+
+
+# Function to find tool changes in a G-code file
+def find_tool_changes(file_path):
+    toolchanges = {}
+
+    # Open the file in read mode
+    with open(file_path, 'r') as f:
+        # Iterate over each line in the file
+        for lineno, line in enumerate(f, start=1):
+            # Use regular expression to find tool changes
+            match = re.match(r'^T(\d)$', line.strip())
+            if match:
+                # Extract tool number from the match
+                toolnr = match.group(1)
+                # Store line number with tool number as key
+                if toolnr not in toolchanges:
+                    toolchanges[toolnr] = []
+                toolchanges[toolnr].append(lineno)
+    return toolchanges
     
-def hex_to_rgb(hex_color):
-    hex_color = hex_color.lstrip('#')
-    red = int(hex_color[0:2], 16) / 255.0
-    green = int(hex_color[2:4], 16) / 255.0
-    blue = int(hex_color[4:6], 16) / 255.0
-    return f"RED={red:.2f} GREEN={green:.2f} BLUE={blue:.2f}"
-
-def find_nozzle_temperature_and_filament_colors(file_path):
+def find_parameters(file_path):
     nozzle_temperatures = {}
     filament_colors = {}
     filament_type = ""
-    with open(file_path, 'r') as file:
-        lines = file.readlines()[-500:]
+    with open(file_path, 'r') as f:
+        lines = f.readlines()[-500:]
         for line in lines:
             if line.startswith("; nozzle_temperature ="):
                 numbers = line.split('=')[1].strip().split(',')
@@ -27,56 +44,130 @@ def find_nozzle_temperature_and_filament_colors(file_path):
             elif line.startswith("; filament_type ="):
                 filvalues = line.split('=')[1].strip().split(';')
                 filament_type = filvalues[0]
-    return nozzle_temperatures, filament_colors, filament_type
+    return nozzle_temperatures, filament_colors, filament_type    
 
-def process_gcode(file_path, nozzle_temperatures, filament_colors, z_offset_value):
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip('#')
+    red = int(hex_color[0:2], 16) / 255.0
+    green = int(hex_color[2:4], 16) / 255.0
+    blue = int(hex_color[4:6], 16) / 255.0
+    return f"RED={red:.2f} GREEN={green:.2f} BLUE={blue:.2f}"
+
+def find_in_file(start_line, regex):
+    with open(file_path, 'r') as f:
+        current_line = 0
+        for line in f:
+            current_line += 1
+            if current_line > start_line:
+                if re.match(regex, line.strip()):
+                    return current_line
+    return 0
+
+def insert_temps_and_leds(file_path):
+    with open(file_path, "r") as f:
+        contents = f.readlines()
+    
+    # Loop through tool numbers
+    modified_lines = {}
+    
+    first_tool_row = find_in_file(0,'^T\d$') # search from the start
+    first_tool = ((contents[first_tool_row-1]).strip())[1]
+    
+    if led_effects: # Turn on leds first tool
+        rgb_value = filament_colors[int(first_tool)]
+        modified_lines[first_tool_row-1] = []
+        modified_lines[first_tool_row-1].append(f"SET_LED LED=T{first_tool}_RGB RED=1 GREEN=1 BLUE=1 INDEX=1 TRANSMIT=1 ; m\n")
+        modified_lines[first_tool_row-1].append(f"SET_LED LED=T{first_tool}_RGB RED=1 GREEN=1 BLUE=1 INDEX=2 TRANSMIT=1 ; m\n")  
+        modified_lines[first_tool_row-1].append(f"SET_LED LED=T{first_tool}_RGB {rgb_value} INDEX=3 TRANSMIT=1 ; m\n") 
+    
+    for toolnr, line_numbers in toolchanges.items():
+        # Iterate through line numbers
+        for i, lineno in enumerate(line_numbers):
+            # Cooldown - Check if the heater won't be used in along time or not at all
+            if lineno==line_numbers[-1]: # Last hit, won't be used again?
+                next_tool_found = 0
+                next_tool_row = find_in_file(lineno,'^T\d$')
+                if next_tool_found == 0 and next_tool_row > 0: # Checking if there'll be another toolchange
+                    modified_lines[next_tool_row-1] = []
+                    modified_lines[next_tool_row-1].append(f"M104 T{toolnr} S0 ; Turning off tool as it's not used again\n") 
+                    if led_effects: # Turn off leds for unused heaters
+                        modified_lines[next_tool_row-1].append(f"SET_LED LED=T{toolnr}_RGB RED=0 GREEN=0 BLUE=0 INDEX=1 TRANSMIT=1 ; m\n")
+                        modified_lines[next_tool_row-1].append(f"SET_LED LED=T{toolnr}_RGB RED=0 GREEN=0 BLUE=0 INDEX=2 TRANSMIT=1 ; m\n")  
+                        modified_lines[next_tool_row-1].append(f"SET_LED LED=T{toolnr}_RGB RED=0 GREEN=0 BLUE=0 INDEX=3 TRANSMIT=1 ; m\n")                               
+                    next_tool_found = 1
+            else:
+                next_tool_row = find_in_file(lineno,'^T\d$')
+                if next_tool_row > 0:
+                    if line_numbers[i+1] - next_tool_row > cooldown_rows: # If next hit of current tool is > cooldown_rows from next toolchange
+                        modified_lines[next_tool_row-1] = []
+                        modified_lines[next_tool_row-1].append(f"M104 T{toolnr} S{cooldown_temp} ; m Cooling down while tool is in long-time parking\n")                
+
+            # Pre-heat - Set temps ahead of toolhead usage
+            print_start = find_in_file(0, '(?i)print_start')
+            if lineno-preheat_rows < print_start:
+                preheat_row = print_start
+            else:
+                preheat_row = lineno - preheat_rows
+                #print(f"current line: {lineno}, start preheat at: {preheat_row}")
+            modified_lines[preheat_row] = []
+            s_value = nozzle_temperatures[int(toolnr)]
+            modified_lines[preheat_row].append(f"M104 T{toolnr} S{s_value} ; Pre-heating tool before use\n")
+            if led_effects: # Sets T?_RGB Led 3 to filament color, and led 1+2 to white
+                rgb_value = filament_colors[int(toolnr)]
+                modified_lines[preheat_row].append(f"SET_LED LED=T{toolnr}_RGB RED=1 GREEN=1 BLUE=1 INDEX=1 TRANSMIT=1 ; m\n")
+                modified_lines[preheat_row].append(f"SET_LED LED=T{toolnr}_RGB RED=1 GREEN=1 BLUE=1 INDEX=2 TRANSMIT=1 ; m\n")  
+                modified_lines[preheat_row].append(f"SET_LED LED=T{toolnr}_RGB {rgb_value} INDEX=3 TRANSMIT=1 ; m\n")
+    
+    myKeys = list(modified_lines.keys())
+    myKeys.sort()
+    sorted_dict = {i: modified_lines[i] for i in myKeys}
+    
+    indexcounter = 0
+    for i,kv in sorted_dict.items():
+        print(f"Line {i}: inserting {kv}")
+        contents.insert(i+indexcounter, "".join(kv))
+        indexcounter += 1
+    
+    with open(file_path, "w") as f:
+        contents = "".join(contents)
+        f.write(contents)        
+
+def process_gcode_offset(file_path, z_offset_value):
     modified_lines = []
     z_move_regex = re.compile(r"(G[01]\s.*Z)([-\+]?\d*\.?\d*)(.*)")  
-    t_pattern = re.compile(r'^T\d$')    
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
         for i in range(len(lines) - 1):
-            match = re.match(t_pattern, lines[i])
-            if match:        
-                t_number = lines[i].strip()[1:]
-                s_value = nozzle_temperatures[int(t_number)]
-                rgb_value = filament_colors[int(t_number)]
-                modified_lines.append(f"M104 T{t_number} S{s_value}\n")
-                if led_effects: # Sets T?_RGB Led 3 to filament color, and led 1+2 to white
-                    modified_lines.append(f"SET_LED LED=T{t_number}_RGB RED=1 GREEN=1 BLUE=1 INDEX=1 TRANSMIT=1\n")
-                    modified_lines.append(f"SET_LED LED=T{t_number}_RGB RED=1 GREEN=1 BLUE=1 INDEX=2 TRANSMIT=1\n")  
-                    modified_lines.append(f"SET_LED LED=T{t_number}_RGB {rgb_value} INDEX=3 TRANSMIT=1\n")                
-            if z_offset_adjust:
-                if lines[i].startswith("G") and "Z" in lines[i]:
-                    result = z_move_regex.fullmatch(lines[i].strip())
-                    if result:
-                        adjusted_z = round(float(result.group(2)) + z_offset_value, 5)
-                        lines[i] = result.group(1) + str(adjusted_z) + result.group(3) + " ; adjusted by z offset\n"
+            if lines[i].startswith("G") and "Z" in lines[i]:
+                result = z_move_regex.fullmatch(lines[i].strip())
+                if result:
+                    adjusted_z = round(float(result.group(2)) + z_offset_value, 5)
+                    lines[i] = result.group(1) + str(adjusted_z) + result.group(3) + " ; m adjusted by z offset\n"
             modified_lines.append(lines[i])
-    return modified_lines
-
-def turn_off_finished_heaters(file_path):
-    pattern = re.compile(r'^T\d$')
-
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-
-    last_indices = {}
-    for i, line in enumerate(lines):
-        match = re.match(pattern, line)
-        if match:
-            key = match.group()
-            last_indices[key] = i
-
-    for key, index in last_indices.items():
-        next_index = index + 1
-        while next_index < len(lines) and not pattern.match(lines[next_index]):
-            next_index += 1
-        if next_index < len(lines):
-            lines.insert(next_index, f'M104 S0 ; Turning off heater as it\'s not used anymore\n')
-
+            
     with open(file_path, 'w') as file:
-        file.writelines(lines)
+        file.writelines(modified_lines)
+
+def remove_m104_lines(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    filtered_lines = [line for line in lines if not line.startswith("M104 S")]
+    with open(file_path, 'w') as f:
+        f.writelines(filtered_lines)
+        
+def fix_print_start(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    sorted_tools = sorted(toolchanges.keys())
+    start_line_index = find_in_file(0, '(?i)print_start')
+    start_line = lines[start_line_index-1].strip()
+    for t in sorted_tools:
+        s_value = nozzle_temperatures[int(t)]
+        start_line = start_line + " T" + "".join(t) + "_TEMP=" + str(s_value)
+    lines[start_line_index-1] = start_line + '\n'
+            
+    with open(file_path, 'w') as f:
+        f.writelines(lines)        
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -84,19 +175,24 @@ if __name__ == "__main__":
         sys.exit(1)
         
     file_path = sys.argv[1]
-    turn_off_finished_heaters(file_path) 
-    nozzle_temperatures, filament_colors, filament_type = find_nozzle_temperature_and_filament_colors(file_path)
-    
-    if "ASA" in filament_type or "ABS" in filament_type:
-        z_offset_value = 0.01
-    elif "PLA" in filament_type:
-        z_offset_value = 0.06
-    elif "TPU" in filament_type or "PET" in filament_type or "PETG" in filament_type:
-        z_offset_value = 0.12
-    else:
-        z_offset_value = 0.0
-    
-    modified_lines = process_gcode(file_path, nozzle_temperatures, filament_colors, z_offset_value)
-   
-    with open(file_path, 'w') as file:
-        file.writelines(modified_lines)
+
+    toolchanges = find_tool_changes(file_path)
+    nozzle_temperatures, filament_colors, filament_type = find_parameters(file_path)    
+    fix_print_start(file_path)
+    remove_m104_lines(file_path)    
+    insert_temps_and_leds(file_path)
+
+    if z_offset_adjust:
+        if "ASA" in filament_type or "ABS" in filament_type:
+            z_offset_value = 0.01
+        elif "PLA" in filament_type:
+            z_offset_value = 0
+        elif "TPU" in filament_type or "PET" in filament_type or "PETG" in filament_type:
+            z_offset_value = 0.12
+        else:
+            z_offset_value = 0
+        
+        if not z_offset_value == 0:
+            process_gcode_offset(file_path, z_offset_value)
+
+
